7b70641591323d857202a23b6ff88561858b54e9
WilliamBritoFreitas <carteirodowilliam@gmail.com>
2026-05-23 17:59:00 -0300

    comit 2

---
+++ scripts/Dashfin.js
@@ -0,0 +1,12 @@
+const lista_movimentacao=[];
+
+const data_mov = document.getElementById("data_mov");
+const descricao = document.getElementById("descricao");
+const tipo_entrada = document.getElementById("tipo_entrada")
+const valor = document.getElementById("valor")
+
+function btn_acrescentar () {
+    const linha_mov [
+        
+    ]
+}
